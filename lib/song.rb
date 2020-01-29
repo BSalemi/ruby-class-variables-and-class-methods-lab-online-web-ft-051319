25e60d2b521d1c1e69547121989b4d5c51128be4
Brian Salemi <briandsalemi@gmail.com>
2020-01-29 16:47:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,28 +4,28 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre
     @@artists << artist
+    @@genres << genre
   end
 
   def self.count
     @@count
   end
 
-  def self.genres
-    @@genres.uniq
-  end
-
   def self.artists
     @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
   end
 
   def self.genre_count
@@ -37,7 +37,7 @@ class Song
         genre_hash[genre] = 1
       end
     end
-    genre_hash
+      genre_hash
   end
 
   def self.artist_count
