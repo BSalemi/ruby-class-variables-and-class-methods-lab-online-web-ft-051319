441eacb38dc414603f88077ae0ba995f6612f3b5
Brian Salemi <briandsalemi@gmail.com>
2020-01-29 16:14:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 class Song
-
   attr_accessor :name, :artist, :genre
 
   @@count = 0
@@ -10,7 +9,6 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-
     @@count += 1
   end
 
